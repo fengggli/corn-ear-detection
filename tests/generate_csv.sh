a3e2a830c2d84aa513641525fefd9325fc9c1b5f
Feng Li <fengggli@yahoo.com>
2019-12-01 17:54:46 -0500

    use stepsize 1

--- tests/generate_csv.sh
+++ tests/generate_csv.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 FILES=/share/Competition2/cornvideos/4inch/*.MP4
+# cannot read 2inch video in sievert, why?
 
 for file in $FILES
 do
   echo "process video $file"
-  python3 tests/detect.py  --videopath=$file --stepsize 10
+  #python3 tests/detect.py  --videopath=$file --stepsize 10
+  python3 tests/detect.py  --videopath=$file --stepsize 1 --norender
 done
