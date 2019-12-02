6f601f07107079acba3e4adfc4c0f2fd5b607c27
Feng Li <fengggli@yahoo.com>
2019-12-02 15:45:18 -0500

    minimal example

--- tests/test_cv.py
+++ tests/test_cv.py
@@ -1,12 +1,14 @@
 import cv2
-video_path='./GOPR0165.MP4'
+import numpy as np
+video_path= '/share/Competition2/cornvideos/2inch/GOPR0165.MP4'
+#video_path= '/share/Competition2/cornvideos/4inch/GOPR0388.MP4'# 4 inch is fine
 cap = cv2.VideoCapture(video_path)
 # cap = cv2.VideoCapture("test.avi") # it's also not working
 if not cap.isOpened(): # Output: False
     print("Open error")
 
-ret = cap.read()
+ret, image_np = cap.read()
 if not ret:
     print("Read error")
 else:
-    print("Fine")
+    print("Fine, contents:", image_np)
