23c0e267b13bda7d3c1940fb9432aa44b8520752
Feng Li <fengggli@yahoo.com>
2019-12-04 21:57:53 -0500

    2inch file is fine after muting the audio

--- tests/generate_csv.sh
+++ tests/generate_csv.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-FILES=/share/Competition2/cornvideos/4inch/*.MP4
-# cannot read 2inch video in sievert, why?
+#FILES=/share/Competition2/cornvideos/4inch/*.MP4
+FILES=/share/Competition2/cornvideos/2inch_muted/*.MP4
+# cannot read 2inch video in sievert, why? -> see tests/test_cv.py
 
 for file in $FILES
 do
--- tests/test_cv.py
+++ tests/test_cv.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
-video_path= '/share/Competition2/cornvideos/2inch/GOPR0165.MP4'
+
+# video are all muted, using command: 
+# for file in ./2inch/*; do ffmpeg -i $file -vcodec copy -an 2inch_muted/`basename $file`; done
+# see https://stackoverflow.com/questions/49060054/opencv-videocapture-closes-with-videos-from-gopro
+video_path= '/share/Competition2/cornvideos/2inch_muted/GOPRO165.MP4'
 #video_path= '/share/Competition2/cornvideos/4inch/GOPR0388.MP4'# 4 inch is fine
 cap = cv2.VideoCapture(video_path)
 # cap = cv2.VideoCapture("test.avi") # it's also not working
